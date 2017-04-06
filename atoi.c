5240f5490941af39f527d07503fb1950fbcc14d0
counry <znhjf@126.com>
2017-04-06 10:23:00 +0800

    atoi and itoa

--- atoi.c
+++ atoi.c
@@ -1,1 +1,44 @@
 /*实现字符串转整数的代码*/
+
+
+#include <stdio.h>
+
+isspace(unsigned char x)  
+{  
+    if(x==' '||x=='\t'||x=='\n'||x=='\f'||x=='\b'||x=='\r')  
+        return 1;  
+    else    
+        return 0;  
+}
+
+isdigit(unsigned char x)  
+{  
+    if(x<='9'&&x>='0')           
+        return 1;   
+    else   
+        return 0;  
+}  
+
+int atoi(const char *nptr, char **endptr)
+{
+    int c = 0;
+    int total =;
+    int sign = 1;
+
+    while(isspace((unsigned char)*nptr))
+        ++nptr;
+    if (*nptr == '-' || *nptr == '+')
+    {
+        if (*str == '-')
+            sign = -1;
+        nptr++;
+    }
+    total = 0;
+    c = *nptr;
+    while(isdigit(c)) {
+        total = 10 * total + (c - '0');
+        nptr++;
+        c = *nptr;
+    }
+    return total * sign;
+}
--- itoa.c
+++ itoa.c
@@ -1,1 +1,36 @@
 /*实现数字转字符串*/
+
+char *reverse(char *s)  
+{  
+    char temp;  
+    char *p = s;    //p指向s的头部  
+    char *q = s;    //q指向s的尾部  
+    while(*q)  
+        ++q;  
+    q--;  
+
+    //交换移动指针，直到p和q交叉  
+    while(q > p)  
+    {  
+        temp = *p;  
+        *p++ = *q;  
+        *q-- = temp;  
+    }  
+    return s;  
+}  
+
+void itoa(int n, char s[])
+{
+    int i,j,sign;
+    if((sign = n) < 0)
+        n = n * -1;
+    i = 0;
+    do {
+        s[i++] = n % 10 + '0';
+    } while((n/=10)>0);
+
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
