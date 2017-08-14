82afcb3dab1d08ce8dff1a14eced2ca16aca77b4
zhounianhua <zhounianhua@meituan.com>
2017-08-14 13:12:34 +0800

    commit

--- cpp/setcmp.cpp
+++ cpp/setcmp.cpp
@@ -18,6 +18,7 @@
 
 #include <iostream>
 #include <set>
+#include <sstream>
 
 using namespace std;
 
@@ -31,4 +32,52 @@ inline void PRINT_ELEM(const T& coll, const char* optcstr="")
     }
     std::cout << std::endl;
 }
+
+class range {
+public:
+    range(long start, long end, int seq):m_start(start),m_end(end),m_seq(seq){}
+    std::string get_range_str() const
+    {
+        std::stringstream oss;
+        oss.str("");
+        oss << "range("<<m_start<<","<<m_end<<"): seq(" << m_seq<<")";
+        return std::string(oss.str());
+    }
+    long m_start;
+    long m_end;
+    int m_seq;
+
+};
+
+bool compare(const range& lhs, const range& rhs)
+{
+    return lhs.m_end < rhs.m_end;
+}
+
+int main(void)
+{
+    range r1(10,50,1);
+    range r2(51,60,2);
+    range r3(61,70,3);
+    range r4(71,80,4);
+    range r5(81,90,5);
+    range r(73,73,0);
+    std::set<range, decltype(compare)*> iset(compare);
+    iset = {r1,r2,r3,r4,r5};
+    auto ret = iset.find(r);
+    if (ret != iset.end()) {
+        std::cout << "find:" << ret->get_range_str() << std::endl;
+    }
+    ret = iset.lower_bound(r);
+    if (ret != iset.end()) {
+        std::cout << "lower_bound:" << ret->get_range_str() << std::endl;
+    }
+    ret = iset.upper_bound(r);
+    if (ret != iset.end()) {
+        std::cout << "upper_bound:" << ret->get_range_str() << std::endl;
+    }
+    return 0;
+}
+
+
 
